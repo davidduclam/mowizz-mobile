4244b4e9c625aa08b72c802c0eff9ff33ac8dd73
David Duc Lam <davla1907@gmail.com>
2026-01-17 14:46:32 +0100

    Add MovieCard component for displaying movie title and poster; implement navigation and layout for Home and Search screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,19 @@
-import { Stack } from "expo-router";
-import './globals.css';
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon
+          sf={{ default: "house", selected: "house.fill" }}
+          drawable="custom_home_drawable"
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="search" role="search">
+        <Label>Search</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,83 @@
-import { Text, View } from "react-native";
+import MovieCard from "@/components/MovieCard";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  type Movie = {
+    id: string;
+    title: string;
+    release_date: string;
+    poster_path: string;
+    overview: string;
+  };
+
+  type MoviesResponse = Movie[];
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Movie[]>([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/movie/popular");
+      const json = (await response.json()) as MoviesResponse;
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500 font-bold">MoWizz!</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-black">
+      <StatusBar style="light" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Text className="text-white font-bold text-4xl">MoWizz</Text>
+
+        <View>
+          {isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            <View className="flex-1 mt-5">
+              <Text className="text-white text-lg font-bold mt-5 mb-3">
+                Popular Movies
+              </Text>
+              <FlatList
+                className="mt-2 pb-32"
+                data={data}
+                numColumns={3}
+                scrollEnabled={false}
+                keyExtractor={({ id }) => id}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+              />
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
